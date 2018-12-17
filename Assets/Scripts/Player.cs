963f5c2927f53b131b00ac367b38aee3c293ff11
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-12-17 15:36:24 -0500

    Added players

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    KeyCode left = KeyCode.A;
+    KeyCode right = KeyCode.D;
+
+    float speed = 10f;
+
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if(Input.GetKey(left))
+        {
+            rigidBody.AddForce(new Vector2(-10f, 0f));
+        }
+        if (Input.GetKey(left))
+        {
+            rigidBody.AddForce(new Vector2(10f, 0f));
+        }
+    }
+}
